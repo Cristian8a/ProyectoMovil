c57c5b9560de854ade0284501020462c1c114747
Cristian8a <cristian.ochoa@iteso.mx>
2023-12-01 20:30:43 -0600

    redo

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.indigo,
           ),
         ),
-        home: AskQuestions(),
+        home: HomePage(),
       ),
     );
   }
